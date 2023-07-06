8217b5f4583e755bb921dfda8897696f43d764ff
otaxhu <oscarperniamoreno@gmail.com>
2023-07-06 09:14:58 -0400

    grouping routes and serving static files in the ./static folder

--- internal/rest/rest_gin.routes.go
+++ internal/rest/rest_gin.routes.go
@@ -3,5 +3,11 @@ package rest
 // Funcion que inicializa las rutas con handlers definidos en el struct ginMovieHandler.
 // Es responsabilidad del programador inicializar las rutas con sus respectivos handlers.
 func (ga *ginApp) bindRoutes() {
-	ga.app.POST("/movies", ga.movieHandler.PostMovie)
+
+	movieRoutes := ga.app.Group("/movies")
+
+	movieRoutes.POST("", ga.movieHandler.PostMovie)
+	movieRoutes.GET("", ga.movieHandler.GetMovies)
+
+	ga.app.Static("/static", "./static")
 }
