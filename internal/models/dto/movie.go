dfa883f0c703ad727e2d0281250a9630b60dfb12
otaxhu <oscarperniamoreno@gmail.com>
2023-07-06 09:10:01 -0400

    movie DTOs

---
+++ internal/models/dto/movie.go
@@ -0,0 +1,22 @@
+package dto
+
+import (
+	"mime/multipart"
+	"time"
+)
+
+// This is a representation of the movie in the service layer when you want to save it.
+// The reason of this is because I want the gin framework decoupled from my business logic.
+// So I want the Cover file decoupled of it and save it by myself.
+type SaveMovie struct {
+	Title string                `validate:"required"`
+	Date  time.Time             `validate:"required"`
+	Cover *multipart.FileHeader `validate:"required"`
+}
+
+type GetMovies struct {
+	Id       int       `json:"id"`
+	Title    string    `json:"title"`
+	Date     time.Time `json:"date"`
+	CoverUrl string    `json:"cover_url"`
+}
